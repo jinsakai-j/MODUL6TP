9d8ba071d65573658bfa1cb9bce469bb096a174f
Rajaa Azharul <azharulrajaa@gmail.com>
2026-04-05 22:48:05 +0700

    menambahkan class SayaTrackMusic

--- TP_MODUL6_103022400116-/Program.cs
+++ TP_MODUL6_103022400116-/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using TP_MODUL6_103022400116;
+
+namespace TP_MODUL6_103022400116
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayaMusicTrack lagu1 = new SayaMusicTrack("Danger Line");
+
+            lagu1.PrintTrackDetails();
+            lagu1.IncreasePlayCount(5000);
+            lagu1.PrintTrackDetails();
+        }
+    }
+}
---
+++ TP_MODUL6_103022400116-/SayaMusicTrack.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TP_MODUL6_103022400116
+{
+    public class SayaMusicTrack
+    {
+        private int id;
+        private int playCount;
+        private string title;
+
+        public SayaMusicTrack(string title)
+        {
+            Random random = new Random();
+            this.id = random.Next(10000, 100000); // 5 digit
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            playCount += count;
+        }
+
+        public void PrintTrackDetails()
+        {
+            Console.WriteLine("Track ID     : " + id);
+            Console.WriteLine("Track Title  : " + title);
+            Console.WriteLine("Play Count   : " + playCount);
+            Console.WriteLine("-----------------------------------");
+        }
+    }
+}
